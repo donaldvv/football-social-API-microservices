9a7685827d30d7d5697c51fc585d88e7b18a2a59
donaldvv <donaldveizi3693@gmail.com>
2022-05-14 20:02:21 +0200

    fixed the yaml file

--- users-service/src/main/java/com/don/usersservice/controller/UsersMsStatusController.java
+++ users-service/src/main/java/com/don/usersservice/controller/UsersMsStatusController.java
@@ -3,22 +3,19 @@ package com.don.usersservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.Min;
 
 @RestController
 @Validated
 @RequiredArgsConstructor
 //@SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/users")
-public class UserController {
+public class UsersMsStatusController {
 
     @GetMapping("/status")
     public String getStatus() {
-        return "AAAAAAAAAAAAAAAAAA";
+        return "Users Microservice is up and working :) ";
     }
 
 }
