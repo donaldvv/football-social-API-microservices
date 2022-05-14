d258356fae3cc03429639e021dda81bf6f02f4a3
donaldvv <donaldveizi3693@gmail.com>
2022-05-14 16:00:33 +0200

    first commit

---
+++ users-service/src/main/java/com/don/usersservice/UsersServiceApplication.java
@@ -0,0 +1,28 @@
+package com.don.usersservice;
+
+import feign.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients   // necessary to use the @FeignClient
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+    // besides the config in the app.properties file, in order to do logging for Feign Clients, we also need to set the level here as a Bean
+    @Bean
+    @Profile("!production")
+    Logger.Level feignDefaultLoggerLevel()
+    {
+        return Logger.Level.FULL;
+    }
+
+}
