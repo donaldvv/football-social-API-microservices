c19bb75ea026f6813986c3194f2df7bac8bbcf0a
donaldvv <donaldveizi3693@gmail.com>
2022-05-18 20:51:05 +0200

    did stuff xD

--- users-service/src/main/java/com/don/usersservice/model/User.java
+++ users-service/src/main/java/com/don/usersservice/model/User.java
@@ -1,11 +1,18 @@
 package com.don.usersservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import java.util.List;
 
@@ -20,7 +27,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 55, unique = true)
     @Email
     private String email;
 
